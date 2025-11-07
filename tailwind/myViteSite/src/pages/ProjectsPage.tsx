71c206cad0b4db88c75f14118bfdca7be65d97a0
Deshaun Brown <deshaunbrown933@yahoo.com>
2025-11-07 16:10:50 -0600

    Update ProjectsPage.tsx

--- tailwind/myViteSite/src/pages/ProjectsPage.tsx
+++ tailwind/myViteSite/src/pages/ProjectsPage.tsx
@@ -2,7 +2,7 @@ export default function Projects() {
   return (
     <main className="pt-16 px-4">
       <h1 className="text-2xl font-bold mb-4">Featured Projects</h1>
-      <Projects/>
+      <Projects />
     </main>
   );
 }
