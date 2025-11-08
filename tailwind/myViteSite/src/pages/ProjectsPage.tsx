02d0ead1831d8bba1ec2bffa948216bd1c8d5fc4
Deshaun Brown <deshaunbrown933@yahoo.com>
2025-11-07 20:13:54 -0600

    Commit Summary

--- tailwind/myViteSite/src/pages/ProjectsPage.tsx
+++ tailwind/myViteSite/src/pages/ProjectsPage.tsx
@@ -1,4 +1,6 @@
-export default function Projects() {
+import Projects from "../components/Projects";
+
+export default function ProjectsPage() {
   return (
     <main className="pt-16 px-4">
       <h1 className="text-2xl font-bold mb-4">Featured Projects</h1>
